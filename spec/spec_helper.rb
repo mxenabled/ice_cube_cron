ddbde840c6298a2e1cdf388afea07dbe2a89b522
matthew-nichols <matthew.nichols@mx.com>
2015-11-03 15:29:06 -0700

    Add code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,13 @@
 require 'ice_cube_cron'
 require 'pry'
 
+require 'simplecov'
+SimpleCov.start
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
